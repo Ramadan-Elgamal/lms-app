21784e2ba10093667b76abec83500a1290d88677
ramadan.reda12345 <ramadan.reda12345@gmail.com>
2025-02-28 12:18:37 +0000

    created the components

---
+++ client/src/components/educator/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>efu Footer</div>
+  )
+}
+
+export default Footer
---
+++ client/src/components/educator/NavBar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div>edu NavBar</div>
+  )
+}
+
+export default NavBar
