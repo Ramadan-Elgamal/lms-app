eb8fe6a5199b08c2ce4d26284718b729f270a023
ramadan.reda12345 <ramadan.reda12345@gmail.com>
2025-03-19 11:24:39 +0000

    add the functions for the courses section

--- client/src/components/student/CourseCard.jsx
+++ client/src/components/student/CourseCard.jsx
@@ -4,7 +4,7 @@ import { AppContext } from '../../context/AddContext'
 import { Link } from 'react-router-dom'
 
 const CourseCard = ({course}) => {
-  const {currency} = useContext(AppContext)
+  const {currency, calculateRating} = useContext(AppContext)
   return (
     <Link to={`/course/${course._id}`} onClick={() => scrollTo(0,0,)} className='border border-gray-500/30 pt-6 overflow-hidden rounded-large '>
       <img src={course.courseThumbnail} alt="" className='w-full '/>
@@ -12,7 +12,7 @@ const CourseCard = ({course}) => {
         <h3 className='text-base font-semibold'>{course.courseTitle}</h3>
         <p className='text-gray-500'>{course.educator.name}</p>
         <div className='flex items-center space-x-2'>
-          <p>4.5</p>
+          <p>{calculateRating(course)}</p>
           <div className='flex'>
             {/* {[...Array(5).map((_,i) => (<img key={i} src={assets.star} alt='' className='w-3.5 h-3.5' />))]} */}
 {
@@ -21,7 +21,7 @@ Array(5)
     .map((_, i) => (
       <img
         key={i}
-        src={assets.star}
+        src={i < Math.floor(calculateRating(course)) ? assets.star : assets.star_blank}
         alt="star" // Provide a meaningful alt text
         className="w-3.5 h-3.5"
       />
