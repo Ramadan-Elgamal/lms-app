03e4a0bf8ed46e5741d9a223e5d5b57f32845eef
ramadan.reda12345 <ramadan.reda12345@gmail.com>
2025-04-19 13:06:25 +0000

    finish the courseDetails page

--- client/src/components/student/CourseCard.jsx
+++ client/src/components/student/CourseCard.jsx
@@ -1,39 +1,51 @@
-import React, { useContext } from 'react'
-import { assets } from '../../assets/assets'
-import { AppContext } from '../../context/AddContext'
-import { Link } from 'react-router-dom'
+import React, { useContext } from "react";
+import { assets } from "../../assets/assets";
+import { AppContext } from "../../context/AddContext";
+import { Link } from "react-router-dom";
 
-const CourseCard = ({course}) => {
-  const {currency, calculateRating} = useContext(AppContext)
+const CourseCard = ({ course }) => {
+  const { currency, calculateRating } = useContext(AppContext);
   return (
-    <Link to={`/course/${course._id}`} onClick={() => scrollTo(0,0,)} className='border border-gray-500/30  overflow-hidden rounded-large '>
-      <img src={course.courseThumbnail} alt="" className='w-full '/>
-      <div className='p-3 text-left'>
-        <h3 className='text-base font-semibold'>{course.courseTitle}</h3>
-        <p className='text-gray-500'>{course.educator.name}</p>
-        <div className='flex items-center space-x-2'>
+    <Link
+      to={`/course/${course._id}`}
+      onClick={() => scrollTo(0, 0)}
+      className="border border-gray-500/30  overflow-hidden rounded-large "
+    >
+      <img src={course.courseThumbnail} alt="" className="w-full " />
+      <div className="p-3 text-left">
+        <h3 className="text-base font-semibold">RB3</h3>
+        <p className="text-gray-500">RB3</p>
+        <div className="flex items-center space-x-2">
           <p>{calculateRating(course)}</p>
-          <div className='flex'>
+          <div className="flex">
             {/* {[...Array(5).map((_,i) => (<img key={i} src={assets.star} alt='' className='w-3.5 h-3.5' />))]} */}
-{
-Array(5)
-    .fill(null) // Initialize the array with 5 elements
-    .map((_, i) => (
-      <img
-        key={i}
-        src={i < Math.floor(calculateRating(course)) ? assets.star : assets.star_blank}
-        alt="star" // Provide a meaningful alt text
-        className="w-3.5 h-3.5"
-      />
-    ))
-}
+            {Array(5)
+              .fill(null) // Initialize the array with 5 elements
+              .map((_, i) => (
+                <img
+                  key={i}
+                  src={
+                    i < Math.floor(calculateRating(course))
+                      ? assets.star
+                      : assets.star_blank
+                  }
+                  alt="star" // Provide a meaningful alt text
+                  className="w-3.5 h-3.5"
+                />
+              ))}
           </div>
-          <p className='text-gray-500'>{course.courseRatings.length}</p>
+          <p className="text-gray-500">{course.courseRatings.length}</p>
         </div>
-        <p className='text-base font-semibold text-gray-800'>{currency}{(course.coursePrice - course.discount * course.coursePrice /100).toFixed(2)}</p>
+        <p className="text-base font-semibold text-gray-800">
+          {currency}
+          {(
+            course.coursePrice -
+            (course.discount * course.coursePrice) / 100
+          ).toFixed(2)}
+        </p>
       </div>
     </Link>
-  )
-}
+  );
+};
 
-export default CourseCard
+export default CourseCard;
--- client/src/context/AddContext.jsx
+++ client/src/context/AddContext.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { createContext } from "react";
 import { dummyCourses } from '../assets/assets';
 import { useNavigate } from 'react-router-dom';
+import humanizeDuration from 'humanize-duration';
 
 export const AppContext = createContext();
 
@@ -28,11 +29,37 @@ export const AppContextProvider = (props) => {
         return totalRating / course.courseRatings.length
     }
 
+    // function to calculate course chapter time
+    const calculateChapterTime = (chapter) => {
+        let time = 0
+        chapter.chapterContent.map((lecture) => time += lecture.lectureDuration)
+        return humanizeDuration(time * 60 * 1000, {units:["h","m"]})
+    }
+
+    // function to calculate course duration
+    const calculateCourseDuration = (course) => {
+        let time = 0
+        course.courseContent.map((chapter) => chapter.chapterContent.map(
+            (lecture) => time += lecture.lectureDuration
+        ))
+        return humanizeDuration(time * 60 * 1000, {units:["h","m"]})
+    }
+
+    // function to calculate the number of lecture in a course
+    const calculateNoOfLectures = (course) => {
+        let totalLectures = 0 
+        course.courseContent.forEach(chapter => {
+            if(Array.isArray(chapter.chapterContent)){
+                totalLectures += chapter.chapterContent.length
+            }
+        })
+        return totalLectures
+    }
     useEffect(() => {
         fetchAllCourses()
     }, [])
     const value = {
-        currency, allCourses, navigate, calculateRating, isEducator, setIsEducator
+        currency, allCourses, navigate, calculateRating, isEducator, setIsEducator, calculateChapterTime, calculateCourseDuration, calculateNoOfLectures
     }
     return (
         <AppContext.Provider value={value}>
