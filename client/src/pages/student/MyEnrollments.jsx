5120588e2d3f87b7dabb3a00933f98da60afbb87
khaledNasr56 <kn3846228@gmail.com>
2025-04-23 16:39:04 +0200

    Start on Enrollment page

--- client/src/pages/student/MyEnrollments.jsx
+++ client/src/pages/student/MyEnrollments.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const MyEnrollments = () => {
   return (
+   <>
     <div>MyEnrollments</div>
+    <div>MyEnrollments</div>
+    <div>MyEnrollments</div>
+    <div>MyEnrollments</div>
+    <div>MyEnrollments</div>
+    <div>MyEnrollments</div>
+    <div>MyEnrollments</div>
+    <div>MyEnrollments</div>
+   </>
   )
 }
 
