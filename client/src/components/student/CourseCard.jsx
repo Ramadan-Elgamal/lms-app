8219b7823b03015b9bdec642fd4444a913d55aab
ramadan.reda12345 <ramadan.reda12345@gmail.com>
2025-05-08 12:11:03 +0000

    edited the coursecard

--- client/src/components/student/CourseCard.jsx
+++ client/src/components/student/CourseCard.jsx
@@ -13,8 +13,8 @@ const CourseCard = ({ course }) => {
     >
       <img src={course.courseThumbnail} alt="" className="w-full " />
       <div className="p-3 text-left">
-        <h3 className="text-base font-semibold">RB3</h3>
-        <p className="text-gray-500">RB3</p>
+        <h3 className="text-base font-semibold">{course.courseTitle}</h3>
+        <p className="text-gray-500">{course.educator.name}</p>
         <div className="flex items-center space-x-2">
           <p>{calculateRating(course)}</p>
           <div className="flex">
