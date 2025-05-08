c6b48ca57db0f91d01dac7ec664c8a8e02c32676
ramadan.reda12345 <ramadan.reda12345@gmail.com>
2025-05-08 11:02:32 +0000

    completed the backend

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,5 +1,6 @@
 import express from 'express'
-import { getUserData, purchaseCourse, userEnrolledCourses } from '../controllers/userController.js'
+import { addUserRating, getUserCourseProgress, getUserData, purchaseCourse, userEnrolledCourses } from '../controllers/userController.js'
+import { updateRoleToEducator } from '../controllers/educatorController.js'
 
 
 const userRouter = express.Router()
@@ -7,6 +8,10 @@ const userRouter = express.Router()
 userRouter.get('/data', getUserData)
 userRouter.get('/enrolled-courses', userEnrolledCourses)
 userRouter.post('/purchase', purchaseCourse)
+userRouter.post('/update-course-progress', updateRoleToEducator)
+userRouter.post('/get-course-progress', getUserCourseProgress)
+//this is get not post
+userRouter.post('/add-rating', addUserRating)
 
 export default userRouter
 
